79a0a11930f1ef81a01f339683438ce17c4fcd81
nekogakure <156896971+nekogakure@users.noreply.github.com>
2026-02-15 10:05:14 +0900

    ビルド周りのドキュメントその他もろもろを調整

--- scripts/make_image.sh
+++ scripts/make_image.sh
@@ -7,12 +7,11 @@ cd "$(dirname "$0")/.."
 # initfsディレクトリの場所
 INITFS_DIR="fs"
 OUTPUT_IMG="target/swiftCore.img"
-SIZE="128M"
+SIZE="256M"
 
 echo "Creating disk image: $OUTPUT_IMG (Source: $INITFS_DIR)"
 
 # ターゲットディレクトリがない場合は作成
-# shellcheck disable=SC2046
 mkdir -p $(dirname "$OUTPUT_IMG")
 
 # mke2fsが利用可能か確認
@@ -30,4 +29,3 @@ fi
 mke2fs -t ext2 -b 4096 -d "$INITFS_DIR" -L swiftCore -F "$OUTPUT_IMG" "$SIZE"
 
 echo "Done."
-
