31402b93cbddd3b19c3cd3f996e9c518d367623d
nekogakure <156896971+nekogakure@users.noreply.github.com>
2026-02-22 15:03:45 +0900

    ヒープを拡張し、Rustのstdが動くように

--- src/core/mem/allocator.rs
+++ src/core/mem/allocator.rs
@@ -7,7 +7,7 @@ use x86_64::{
 };
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
-pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
+pub const HEAP_SIZE: usize = 32 * 1024 * 1024; // 32 MiB
 
 pub fn init_heap(
     mapper: &mut impl Mapper<Size4KiB>,
