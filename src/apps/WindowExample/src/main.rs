87a650cb082bf6c6e4529160af3442e6baabe09d
tas0dev <156896971+tas0dev@users.noreply.github.com>
2026-05-25 12:48:56 +0900

    WindowExampleの実装を調子絵

--- src/apps/WindowExample/src/main.rs
+++ src/apps/WindowExample/src/main.rs
@@ -1,14 +1,15 @@
+use viewkit::*;
+
 fn main() {
     println!("[WindowExample] start");
 
-    let ui = viewkit::card!()
+    let ui = card!()
         .children([
-            viewkit::text!("Hello from ViewKit").into_elem(),
-            viewkit::text!("(no Kagami IPC in app code)").into_elem(),
+            text!("Hello from ViewKit").into_elem(),
         ])
         .into_elem();
 
-    viewkit::App::new(ui)
+    App::new(ui)
         .title("WindowExample")
         .run();
 }
